362b983a8886286f13602d392c2007d956894a57
Srishti Bhandari <46895152+Srishti-B@users.noreply.github.com>
2019-06-22 22:15:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import sys
-from PyQt5.QtCore import pyqtSlot
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import bgimgw
 
-class groundstat(QMainWindow):
+import time as tm
+import csv
+import datetime
+
+import socket
+from threading import Thread
+from socketserver import ThreadingMixIn
+
+##currenttime = datetime.datetime.now()
+##currenttime = str(currenttime)
+##currenttime_array = currenttime.split(' ')
+##date = currenttime_array[0]
+##time = currenttime_array[1][:8]
+##mode = time.split(':')
+##s1 = int(mode[0])
+##s2 = int(mode[1])
+##s3 = int(mode[2])
+##s1 = s1*10000+s2*100+s3
+##time = s1
+##mode = date.split('-')
+##s1 = int(mode[0])
+##s2 = int(mode[1])
+##s3 = int(mode[2])
+##s1 = s1*10000+s2*100+s3
+##date = s1
+##filename = 'output'+str(time)+'_'+str(date)
+##filename+='.csv'
+##print('Name of File created :',filename)
+##header_name = ["Date","Time","PM1","PM2.5","PM10","NO2","CO2","CO","Humidity","Temperature"]
+##with open(filename, 'w+') as csv_file:
+##    csv_writer = csv.writer(csv_file)
+##    csv_writer.writerow(header_name)
+
+
+class GroundStat(QMainWindow):
+
     def __init__(self):
-        super(groundstat, self).__init__()
+        super(GroundStat, self).__init__()
         loadUi('groundstation.ui', self)
 
         self.pushButton_20.clicked.connect(self.submit)
@@ -22,83 +56,122 @@ class groundstat(QMainWindow):
         self.pushButton_8.clicked.connect(self.storedata)
         self.pushButton_7.clicked.connect(self.fpv1)
         self.pushButton_9.clicked.connect(self.fpv2)
-        self.pushButton_10.clicked.connect(self.close)
-        self.message = ""
+        self.pushButton_10.clicked.connect(self.closed)
 
-
-        #Initialization of line edit
-        self.lineEdit = 0.0
-        self.lineEdit_10 = 0.0
-        self.lineEdit_11 = 0.0
-        self.lineEdit_12 = 0.0
-        self.lineEdit_13 = 0.0
-        self.lineEdit_14 = 0.0
-        self.lineEdit_15 = 0.0
-        self.lineEdit_16 = 0.0
-        self.lineEdit_17 = 0.0
-        self.lineEdit_18 = 0.0
-        self.lineEdit_2 = 0.0
-        self.lineEdit_20 = 0.0
-        self.lineEdit_21 = 0.0
-        self.lineEdit_24 = 0.0
-        self.lineEdit_25 = 0.0
-        self.lineEdit_27 = 0.0
-        self.lineEdit_28 = 0.0
-        self.lineEdit_3 = 0.0
-        self.lineEdit_4 = 0.0
-        self.lineEdit_5 = 0.0
-        self.lineEdit_6 = 0.0
-        self.lineEdit_7 = 0.0
-        self.lineEdit_8 = 0.0
-        self.lineEdit_9 = 0.0
-        self.galt = 0.0
-        self.glong = 0.0
-        self.glat = 0.0
-
-
-    def start_server(self):
-        print("Button pressed")
+        self.glatval = 0.0
+        self.glongval = 0.0
+        self.galtval = 0.0
+        self.lat1 = 0.0
+        self.long1 = 0.0
+        self.alt1 = 0.0
+        self.yawd1 = 0.0
+        self.pitchd1 = 0.0
+        self.yawc1 = 0.0
+        self.pitchc1 = 0.0
+        self.yawt1 = 0.0
+        self.pitcht1 = 0.0
+        self.lat2 = 0.0
+        self.long2 = 0.0
+        self.alt2 = 0.0
+        self.yawd2 = 0.0
+        self.pitchd2 = 0.0
+        self.yawc2 = 0.0
+        self.pitchc2 = 0.0
+        self.yawt2 = 0.0
+        self.pitcht2 = 0.0
+        self.mess = " "
 
     def submit(self):
-        print("Button Clickecd")
+        self.glatval = self.glat.toPlainText()
+        self.glongval = self.glong.toPlainText()
+        self.galtval = self.galt.toPlainText()
+        self.mess = "Ground parameters received."
+        self.message.setText(self.mess)
+
+    def postprocessing(self):
+        print("button clicked")
 
     def realtimeprocessing(self):
-        print("Button Clickecd")
-
+        self.mess = "Host Ground Station as a server to the Drone."
+        self.message.setText(self.mess)
     def connectdrone1(self):
-        print("Button Clickecd")
+        print("aa")
 
     def connectdrone2(self):
-        print("Button Clickecd")
+        print("aa")
+        
+    def connectdronea(self,conn1):
+        while True:
+            data = conn1.recv(2048)
+            message = data.decode()
+            message_array = message.split(",")
+            #if (len(message_array) != 7):
+             #   continue
+            print(message)
+
+    def connectdroneb(self,conn2):
+        while True:
+            data = conn2.recv(2048)
+            message = data.decode()
+            message_array = message.split(",")
+            #if (len(message_array) != 7):
+            #   continue
+            print(message)
+
+    def startserver(self):
+        TCP_IP = '192.168.1.104'
+        TCP_PORT = 2004
+        buffersize = 20
+        tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcpServer.bind((TCP_IP, TCP_PORT))
+
+        while True:
+            tcpServer.listen(4)
+            self.mess = "Ground Station Server waiting for Drone1 and Drone2"
+            self.message.setText(self.mess)
+            print(self.mess)
+            (conn1, (ip, port)) = tcpServer.accept()
+            self.mess = "Drone1 connected."
+            self.message.setText(self.mess)
+            Thread(target=self.connectdronea(conn1)).start()
+            (conn2, (ip, port)) = tcpServer.accept()
+            self.mess = "Drone2 connected."
+            self.message.setText(self.mess)
+            Thread(target=self.connectdroneb(conn2)).start()
 
     def openmp1(self):
-        print("Button Clickecd")
+        print("button clicked")
 
     def openmp2(self):
-        print("Button Clickecd")
+        print("button clicked")
 
     def triangulate(self):
-        print("Button Clickecd")
+        print("button clicked")
 
     def display(self):
-        print("Button Clickecd")
+        print("button clicked")
 
     def storedata(self):
-        print("Button Clickecd")
+        self.mess = "Name of the "
+        self.message.setText(self.mess)
 
     def fpv1(self):
-        print("Button Clickecd")
+        print("button clicked")
 
     def fpv2(self):
-        self.val = "button clicked"
-        self.lineEdit_9.setText(self.val)
+        print("button clicked")
 
-    def close(self):
+    def closed(self):
+        for t in threads:
+            t.join()
         sys.exit(0)
 
 app = QApplication(sys.argv)
-window = groundstat()
+window = GroundStat()
 window.show()
 sys.exit(app.exec_())
+
+
 
 
